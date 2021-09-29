a8b0ae27d6defdaee946d6199200fa0a6cf0c248
thomas boyens <thomas.boyens@hotmail.com>
2021-09-29 23:44:36 +0200

    tailwind working

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -6,25 +6,25 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Hallo, Ik ben Thomas.
+            {/* Hi, I'm Thomas. */}
+            <br className="hidden lg:inline-block" />
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+             Ik ben een resultaatgerichte IT student aan de AP Hogeschool Antwerpen. Ik ben een snelle leerling en streef constant naar het leren van nieuwe technologieën. 
+            {/* Result-driven IT student at AP Hogeschool. I am based in Antwerp, Belgium. I am a quick learner who constantly strives to learn new technologies. */}
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
+              Contacteer mij
+              {/* Contact me */}
             </a>
             <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
+              Bekijk mijn projecten
             </a>
           </div>
         </div>
